772f80a042a63be10e901498e90b720275e19fae
Vincent Lejeune <vljn@ovi.com>
2014-06-21 00:27:23 +0200

    Implemented bilateral for compute path.

--- src/graphics/shaders.hpp
+++ src/graphics/shaders.hpp
@@ -645,7 +645,7 @@ class ComputeGaussian17TapHShader
 {
 public:
     static GLuint Program;
-    static GLuint uniform_source, uniform_dest;
+    static GLuint uniform_source, uniform_depth, uniform_dest;
 
     static void init();
 };
@@ -684,7 +684,7 @@ class ComputeGaussian17TapVShader
 {
 public:
     static GLuint Program;
-    static GLuint uniform_source, uniform_dest;
+    static GLuint uniform_source, uniform_depth, uniform_dest;
 
     static void init();
 };
